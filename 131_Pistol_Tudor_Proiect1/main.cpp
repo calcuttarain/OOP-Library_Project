ab9b08942222027d2bf62587b0e50a23bbe53bf6
calcutta.rain <tudor.pistol@gmail.com>
2023-03-06 23:15:05 +0200

    Initial Commit

---
+++ 131_Pistol_Tudor_Proiect1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  131_Pistol_Tudor_Proiect1
+//
+//  Created by Tudor Pistol on 06.03.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
